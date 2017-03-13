9fc7cf572cf51a15b9d166949bcc0ec613f459ed
Evgeny Tarasenko <e.tarasenko@octavianonline.com>
2017-03-13 16:52:41 +0300

    add test test_dealer_take_100_from_player_dollars add cash prop for Player

--- homework/blackjack-tests.py
+++ homework/blackjack-tests.py
@@ -7,7 +7,7 @@ from blackjack import PlayerBuilder
 
 class BlackJackTests(unittest.TestCase):
 	def test_dealer_call_security_if_player_younger_18_years_old_buying_chips(self):
-		player = PlayerBuilder().withName("Peter").withAge(17).create()
+		player = PlayerBuilder().withAge(17).create()
 		dealer = Dealer()
 		res = False
 		try:
@@ -16,6 +16,14 @@ class BlackJackTests(unittest.TestCase):
 			res = True
 
 		self.assertEqual(res, True)
+
+	def test_dealer_take_100_from_player_dollars(self):
+		player = PlayerBuilder().withCash(100).create()
+		dealer = Dealer()
+
+		dealer.buyChips(player, 100)
+
+		self.assertEqual(player.cash, 0)
 
 if __name__ == '__main__':
 	unittest.main()
--- homework/blackjack.py
+++ homework/blackjack.py
@@ -6,23 +6,34 @@ class Dealer(object):
 		if player.age < 18:
 			raise YoungerPlayer
 
+		player.cash = player.cash - cash
+
 class Player(object):
-	def __init__(self, name, age):
+	def __init__(self, name, age, cash):
 		self._name = name
 		self._age = age
+		self._cash = cash
 
 	@property
 	def age(self):
 		return self._age
 
+	@property
+	def cash(self):
+		return self._cash
+
+	@cash.setter
+	def cash(self, v):
+		self._cash = v
 
 class PlayerBuilder(object):
 	def __init__(self):
 		self._name = "John Smith"
 		self._age = 21
+		self._cash = 0
 
 	def create(self):
-		return Player(self._name, self._age)
+		return Player(self._name, self._age, self._cash)
 
 	def withAge(self, age):
 		self._age = age
@@ -30,4 +41,8 @@ class PlayerBuilder(object):
 
 	def withName(self, name):
 		self._name = name
+		return self
+
+	def withCash(self, cash):
+		self._cash = cash
 		return self
